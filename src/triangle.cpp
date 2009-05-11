6e21ae1d405c2184b33e9b67f7d0dcabd92f5995
Sebastian Arcila-Valenzuela <sebastian@denianita.(none)>
2009-05-10 19:58:14 -0500

    nice things

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -8,8 +8,8 @@ TraceResult IntersectTriangle(TriangleProperties &triangle, Ray &ray)
   Vector edge1, edge2, tvec, pvec, qvec;
   float det, invDet;
     
-  edge1 = VectorSub(*triangle.v2, *triangle.v1);
-  edge2 = VectorSub(*triangle.v3, *triangle.v1);
+  edge1 = VectorSub(*triangle.v2 , *triangle.v1);
+  edge2 = VectorSub(*triangle.v3 , *triangle.v1);
   pvec = VectorCross(ray.direction, edge2);
     
   det = VectorDot(edge1, pvec);
@@ -21,7 +21,7 @@ TraceResult IntersectTriangle(TriangleProperties &triangle, Ray &ray)
     }
   invDet = 1/det;
 
-  tvec = VectorSub(ray.origin, *triangle.v1);
+  tvec = VectorSub(ray.origin , *triangle.v1);
 
   u = VectorDot(tvec, pvec) * invDet;
   if(u < 0 || u > 1)
@@ -53,7 +53,7 @@ TraceResult IntersectTriangle(TriangleProperties &triangle, Ray &ray)
 
 Vector TriangleNormal(TriangleProperties &triangle)
 {
-  Vector edge1 = VectorSub(*triangle.v2, *triangle.v1), edge2 = VectorSub(*triangle.v3, *triangle.v1);
+  Vector edge1 = VectorSub(*triangle.v2 , *triangle.v1), edge2 = VectorSub(*triangle.v3 , *triangle.v1);
   Vector normal = VectorCross(edge1, edge2); 
   VectorNormalize(normal);
   return normal;
